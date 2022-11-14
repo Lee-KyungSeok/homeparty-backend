67349c48b802197a33f91479bcbad6cf0cadb483
Lee-KyungSeok <leeseok8347@gmail.com>
2022-11-14 18:10:32 +0900

    Add Kakao Get Me api

--- identity/src/main/java/identity/kakao/KakaoSocialProviderFetcher.java
+++ identity/src/main/java/identity/kakao/KakaoSocialProviderFetcher.java
@@ -3,11 +3,23 @@ package identity.kakao;
 import identity.domain.aggregates.identity.SocialProvider;
 import identity.domain.aggregates.identity.SocialProviderType;
 import identity.domain.models.SocialProviderFetcher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
+@Slf4j
 public class KakaoSocialProviderFetcher implements SocialProviderFetcher {
+
+    private final KakaoClient kakaoClient;
 
     @Override
     public SocialProvider fetch(SocialProviderType providerType, String providerToken) {
-        return null;
+        var kakaoUser = this.kakaoClient.getMe(providerToken);
+        return new SocialProvider(
+                providerType,
+                kakaoUser.getId(),
+                kakaoUser.getProperties().getNickname(),
+                kakaoUser.getKakaoAccount().getEmail()
+        );
     }
 }
